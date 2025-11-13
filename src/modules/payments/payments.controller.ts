8c92c57361212513165397d3259612f171beef4f
4lecky <lunitaromero1221@gmail.com>
2025-11-12 20:13:11 -0500

    [FEAT] Relación con User y Appoinment añadida en entity payments

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -1,26 +1,4 @@
-// payment.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { Controller } from '@nestjs/common';
 
-@Entity('payments')
-export class Payment {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column()
-  serviceId: string; // ID del SOAT o Revisión
-
-  @Column()
-  serviceType: string; // 'soat' o 'revision'
-
-  @Column('decimal')
-  amount: number;
-
-  @Column()
-  status: string; // 'pending', 'approved', 'rejected'
-
-  @Column()
-  paymentMethod: string; // 'tarjeta', 'pse', 'efectivo'
-
-  @CreateDateColumn()
-  createdAt: Date;
-}
+@Controller('payments')
+export class PaymentsController {}
