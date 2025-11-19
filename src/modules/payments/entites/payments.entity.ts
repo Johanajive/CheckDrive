08d23ac9be1bb5691e8622bf8e33cdd2c5c90b93
4lecky <lunitaromero1221@gmail.com>
2025-11-18 20:46:00 -0500

    [REFACTOR] Reubicación de la entity de payments

---
+++ src/modules/payments/entites/payments.entity.ts
@@ -0,0 +1,39 @@
+// payment.entity.ts
+import { join } from 'path';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, OneToOne } from 'typeorm';
+// import { Appointment } from './appointments.entity';
+// import { User } from './users.entity';
+
+
+@Entity('payments')
+export class Payment {
+  @PrimaryGeneratedColumn()
+  id_payment: number;
+
+  @Column()
+  serviceType: string; // 'soat' o 'revision'
+
+  @Column('decimal')
+  amount: number;
+
+  @Column()
+  status: string; // 'pending', 'approved', 'rejected'
+
+  @Column()
+  paymentMethod: string; // 'tarjeta', 'pse', 'efectivo'
+
+  @Column()
+  date_payment: Date;
+
+
+  // @OneToOne(() => Appointment, appointment => appointment.payments { nullable: false, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  // @joinColumn({ name: 'id_appointment' }) // nombre de la columna FK en la tabla payments
+  // appointment: Appointment; // Descomentar cuando se tenga la entidad Appointment
+
+  // @ManyToOne(() => User, user => user.payments { nullable: false, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  // @joinColumn({ name: 'id_user' }) // nombre de la columna FK en la tabla payments
+  // User: user; // Descomentar cuando se tenga la entidad users
+
+}
+
+
--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Payment } from '../../entites/payments.entity';
+import { Payment } from './entites/payments.entity';
 import { CreatePaymentDto } from './dto/create-payments.dto';
 import { UpdateProductDTO } from './dto/update-payments.dto';
 
