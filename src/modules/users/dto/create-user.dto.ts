73d09f6d2db4897ac2fd7ace5ad84af6d747e872
4lecky <lunitaromero1221@gmail.com>
2025-11-19 20:28:44 -0500

    Conflictos resueltos entre feature/inspectionCenter-module y main

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, Length, Matches, IsEmail } from "class-validator";
+import { IsNotEmpty, IsString, Length, Matches, IsEmail, IsInt, Min, Max } from "class-validator";
 import { RolesEnum } from "src/common/enum/roles.enum";
 
 export class CreateUserDTO {
